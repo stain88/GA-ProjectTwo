e376bb7d287cfbcd5657f6046916ffeae22092d5
stain88 <marcbaghdadi@gmail.com>
2015-11-01 00:51:52 +0000

    Added friendships

--- GiTD/app/controllers/application_controller.rb
+++ GiTD/app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
+  helper_method :request_count
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_path, :alert => exception.message
+  end
+
+  def request_count
+    if current_user
+      current_user.pending_friend_requests_from.map(&:user).size
+    end
   end
 end
--- GiTD/app/controllers/users_controller.rb
+++ GiTD/app/controllers/users_controller.rb
@@ -1,8 +1,22 @@
 class UsersController < ApplicationController
   before_action :set_user, only:[:show]
+  before_action :get_counts, only:[:index]
 
   def index
-    @users = User.all
+    if current_user
+      case params[:people]
+      when "friends"
+        @users = current_user.active_friends
+      when "requests"
+        @users = current_user.pending_friend_requests_from.map(&:user)
+      when "pending"
+        @users = current_user.pending_friend_requests_to.map(&:friend)
+      else
+        @users = User.where.not(id: current_user.id)
+      end
+    else
+      @users = User.all
+    end
   end
   
   def show
@@ -12,5 +26,9 @@ class UsersController < ApplicationController
   private
   def set_user
     @user = User.find_by(username: params[:id])
+  end
+  def get_counts
+    @friend_count = current_user.active_friends.size if current_user 
+    @pending_count = current_user.pending_friend_requests_to.map(&:friend).size if current_user 
   end
 end
