04587e8cec90408798ecf791f4869d0470074b1b
stain88 <marcbaghdadi@gmail.com>
2015-11-01 10:22:14 +0000

    Installed forem

--- GiTD/app/controllers/application_controller.rb
+++ GiTD/app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  def forem_user
+    current_user
+  end
+  helper_method :forem_user
+
   helper_method :request_count
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
