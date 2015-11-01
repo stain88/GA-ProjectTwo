051067c567ca21d3c7d14c020b1abfda814f9ec2
stain88 <marcbaghdadi@gmail.com>
2015-11-01 21:53:35 +0000

    Added relation between games and users/jams

--- GiTD/app/models/jam.rb
+++ GiTD/app/models/jam.rb
@@ -1,2 +1,3 @@
 class Jam < ActiveRecord::Base
+  has_many :games
 end
--- GiTD/app/models/user.rb
+++ GiTD/app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   validates_presence_of :username
   validates_uniqueness_of :username
 
+  has_many :games
   has_many :friendships, dependent: :destroy
   has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
 
