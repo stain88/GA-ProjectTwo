4527cf25cb05829cfd3a275ff861d71f00359e64
stain88 <marcbaghdadi@gmail.com>
2015-11-01 23:01:06 +0000

    New Game form styling, path using either id or game name

--- GiTD/app/controllers/games_controller.rb
+++ GiTD/app/controllers/games_controller.rb
@@ -65,7 +65,7 @@ class GamesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_game
-      @game = Game.find_by(title: params[:id])
+      @game = Game.find_by(title: params[:id]) || Game.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
