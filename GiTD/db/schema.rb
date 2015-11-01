791696c29c92b24e3cd1127888548f535c38e932
stain88 <marcbaghdadi@gmail.com>
2015-11-01 21:54:49 +0000

    Games migration

--- GiTD/db/schema.rb
+++ GiTD/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151101182656) do
+ActiveRecord::Schema.define(version: 20151101195421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -122,6 +122,21 @@ ActiveRecord::Schema.define(version: 20151101182656) do
     t.datetime "updated_at",                      null: false
   end
 
+  create_table "games", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "height"
+    t.integer  "width"
+    t.text     "url"
+    t.integer  "user_id"
+    t.integer  "jam_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "games", ["jam_id"], name: "index_games_on_jam_id", using: :btree
+  add_index "games", ["user_id"], name: "index_games_on_user_id", using: :btree
+
   create_table "jams", force: :cascade do |t|
     t.integer  "title"
     t.string   "theme"
@@ -160,4 +175,6 @@ ActiveRecord::Schema.define(version: 20151101182656) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
+  add_foreign_key "games", "jams"
+  add_foreign_key "games", "users"
 end
