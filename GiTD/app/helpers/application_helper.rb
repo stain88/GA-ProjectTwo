9a0d11dac1ed6353785ed567fa7897ea01c87d49
stain88 <marcbaghdadi@gmail.com>
2015-11-04 17:47:56 +0000

    Fixed create new comment bug

--- GiTD/app/helpers/application_helper.rb
+++ GiTD/app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def get_pic(user, area="100x100")
+    user.profile_pic.blank? ? (gravatar_tag user.email, size: area, d: "identicon") : (image_tag user.profile_pic, size: area.to_s)
+  end
+
 end
