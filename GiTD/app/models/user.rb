eb38c26d92648d3a8e1e686f167367a5160fdf5e
stain88 <marcbaghdadi@gmail.com>
2015-11-13 11:27:47 +0000

    Added username format validation

--- GiTD/app/models/user.rb
+++ GiTD/app/models/user.rb
@@ -2,10 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   validates_presence_of :username
   validates_uniqueness_of :username
+  validates_format_of :username, :with => /\A\w+\z/
 
   acts_as_commontator
   acts_as_voter
